cf796b5d87225ad9456326177f31889fcb24a792
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-07-02 12:38:38 +0200

    Fucking hell
    twat

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "batshit";
+
+            Console.WriteLine("Type a funny joke");
+            string joke = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("'" + joke + "'" +"\nReally. This was the best you could do?\nHow old even are you for fuck's sake?");
+
+            int age;
+
+            age = Convert.ToInt16( Console.ReadLine() );
+
+            Console.WriteLine(age + ".\nYour stupidity is tragic. Is this true?");
+
+            bool stupidity = Convert.ToBoolean( Console.ReadLine() );
+            if( stupidity == true){
+                Console.WriteLine("Well, at least you ain't THAT bad.\nPerhaps your retardation is only minor, or maybe it isn't.");
+                if(age <= 14){
+                    Console.WriteLine("Although since you are " + age + " it kinda explains the whole thing.");
+                } else
+                {
+                    Console.WriteLine("You should be mature by now.\nBut no, you chose to be a retarded kid.");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Let's see, maybe I can help you.\nI'll tell you whether you are totally retarded or not\nbased on a simple question.");
+
+                    string answer;
+                    bool isRetard;
+
+                    Console.WriteLine("Are you dumb as shit? Dumber than a fucking retarded ass donkey?");
+                    answer = Console.ReadLine().ToLower();
+                    if ( answer == "yes")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        isRetard = true;
+                        Console.WriteLine("Ah well, congrats. You are officially retarded.\nPress any key to leave...");
+                        Console.ReadKey(true);
+                        Environment.Exit(0);
+                    }
+
+                }
+            }
+
+            Console.WriteLine("\nI am fucking done with you. \nPress any key to fuck off...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Are you all right? No. He's making the tie in the cab as they're flying up Madison. He finally gets there. He runs up the steps into the church. The wedding is on. And he says, "Watermelon? I thought you said Guatemalan. Why would I marry a watermelon?" Is that a bee joke?
+
+Are you all right? No. He's making the tie in the cab as they're flying up Madison. He finally gets there. He runs up the steps into the church. The wedding is on. And he says, "Watermelon? I thought you said Guatemalan. Why would I marry a watermelon?" Is that a bee joke?
+
+Are you all right? No. He's making the tie in the cab as they're flying up Madison. He finally gets there. He runs up the steps into the church. The wedding is on. And he says, "Watermelon? I thought you said Guatemalan. Why would I marry a watermelon?" Is that a bee joke?
+
+How come you don't fly everywhere? It's exhausting. Why don't you run everywhere? It's faster. Yeah, OK, I see, I see. All right, your turn. TiVo. You can just freeze live TV? That's insane! You don't have that? We have Hivo, but it's a disease. It's a horrible, horrible disease.
+
+Tonight we're talking to Barry Benson. Did you ever think, "I'm a kid from the hive. I can't do this"? Bees have never been afraid to change the world. What about Bee Columbus? Bee Gandhi? Bejesus? Where I'm from, we'd never sue humans. We were thinking of stickball or candy stores. 
